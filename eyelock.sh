7f780993f01678f6203d24d83dc4261248626e20
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2024-07-20 14:24:01 -0300

    Atualizar o

---
+++ eyelock.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import socket
+
+# cloudflare port dictionary
+ports = {
+        "FTP":21,
+        "SSH":22,
+        "HTTP":80,
+        "HTTPS":443,
+        "TCP":445,
+        "MySQL":3306,
+        "SMTP":25,
+        "BGP":179,
+        "NTP":123,
+        "DNS":53,
+        "ISAKMP":500,
+        "SMTP":587,
+        "RDP":3389
+}
+
+host = input("Enter a website URL or IP address: ")
+
+# loop through each item in dictionary and test for error
+for service, port in ports.items():
+
+        try:
+
+# calls the customer variable by method .socket()
+# inside the method calls the arguments .AF_INET e IP
+# socket is TCP, so .SOCK_STREAM
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# set the seconds for connection, same -T in nmap
+# tests the connection with the client. "0" means success and "11" is failed
+                client.settimeout(1)
+                code = client.connect_ex((host, port))
+
+                if code == 0:
+                        print(f"{service} | {port} : OPEN")
+                else:
+                        print(f"{service} | {port} : CLOSED")
+
+        except socket.error as error:
+                print(f"Error connecting {port} : {error}")
+
+        finally:
+                client.close()
